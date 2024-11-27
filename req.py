d1eca24e3ebba1b524516f2d6776d4660b39e860
TheMLEngineer <phenomenalkarthikeyan@gmail.com>
2024-11-27 20:28:11 +0000

    req

--- req.py
+++ req.py
@@ -1,1 +1,13 @@
 print("Hello world")
+
+import requests
+
+x = requests.get("http://httpbin.org/get")
+print(x)
+print(x.headers)
+print(x.headers['Server'])
+
+if x.status_code == 200:
+    print("Success")
+elif x.status_code == 404:
+    print("Error")
